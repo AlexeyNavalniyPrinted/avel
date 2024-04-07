6b413308ade589559116d591bcca13bc52c9650b
AlexeyNavalniyPrinted <117517842+AlexeyNavalniyPrinted@users.noreply.github.com>
2024-04-07 20:23:39 +0300

    fix cockroach connection

--- avel/src/fns.rs
+++ avel/src/fns.rs
@@ -1,38 +1,92 @@
 use std::error::Error;
-use actix_web::{get, HttpResponse, post, Responder, web};
-use actix_web::web::Redirect;
+use std::sync::Arc;
+
+use actix_web::{error, get, HttpResponse, post, Responder, web};
+use actix_web::web::{Data, Redirect};
+use rand::distributions::Alphanumeric;
+use rand::Rng;
 use serde::{Deserialize, Serialize};
+use sqlx::Row;
 use url::Url;
 
-
-pub async fn new_short_link() -> HttpResponse  {
-    HttpResponse::Ok().body("good")
-}
+use crate::AppState;
 
 #[derive(Serialize, Deserialize)]
 struct LinkShort {
     link: String
 }
 
-#[get("/short/{link}")]
-pub async fn short_link(link: web::Path<String>) -> Redirect {
-    let redirect_url = format!("https://{}", link);
-    let link_s = LinkShort { link: redirect_url.to_string() };
-
-    Redirect::to(link_s.link)
-}
-
-#[post("/q")]
-pub async fn save_short_link(link: web::Json<LinkShort>) -> HttpResponse {
-    let _url = match Url::parse(&link.link) {
-        Ok(arg) => {
-            arg
-        }
-        Err(_) => {
-            return HttpResponse::BadRequest().body("Adolf Hitler kill niggers")
+#[get("/{link}")]
+pub async fn short_link(link: web::Path<String>, app_state: Data<Arc<AppState>>) -> Result<Redirect, Box<dyn Error>> {
+    let c = "SELECT full_link FROM links WHERE short_link = $1";
+    let rows = match sqlx::query(c)
+        .bind(link.as_str())
+        .fetch_all(&app_state.cockroachdb_session)
+        .await {
+        Ok(r) => r,
+        Err(e) => {
+            eprintln!("{}", e);
+            return Err(Box::new(error::ErrorServiceUnavailable("The server is unavailable")));
         }
     };
-    HttpResponse::Ok().json(link)
+
+    if let Some(row) = rows.get(0) {
+        println!("{:?}", row.columns());
+
+        if let Some(full_link) = row.get::<Option<String>, &str>("full_link") {
+            return Ok(Redirect::to(full_link));
+        }
+    }
+
+    return Err(Box::new(error::ErrorNotFound(
+        "Not found",
+    )));
+}
+
+
+#[post("/new")]
+pub async fn save_short_link(link: web::Json<LinkShort>, app_state: Data<Arc<AppState>>) -> HttpResponse {
+    let url = match Url::parse(&link.link) {
+        Ok(url) => url,
+        Err(_) => return HttpResponse::BadRequest().body("Link is incorrect"),
+    };
+
+    let c = "SELECT short_link FROM links WHERE full_link = $1";
+    let rows = match sqlx::query(c)
+        .bind(url.as_str())
+        .fetch_all(&app_state.cockroachdb_session)
+        .await {
+        Ok(r) => r,
+        Err(e) => {
+            eprintln!("{}", e);
+            return HttpResponse::InternalServerError().body("Internal server error");
+        }
+    };
+
+    if let Some(row) = rows.get(0) {
+        if let Some(short_l) = row.get::<Option<&str>, &str>("short_link") {
+            return HttpResponse::Ok().body(format!("Existing short link: {}", short_l));
+        }
+    }
+
+    let short_url: String = rand::thread_rng()
+        .sample_iter(&Alphanumeric)
+        .take(12)
+        .map(char::from)
+        .collect();
+
+    let insert_query = "INSERT INTO links (short_link, full_link) VALUES ($1, $2)";
+    match sqlx::query(insert_query)
+        .bind(&short_url)
+        .bind(url.as_str())
+        .execute(&app_state.cockroachdb_session)
+        .await {
+        Ok(_) => HttpResponse::Ok().body(format!("New short link created: {}", short_url)),
+        Err(e) => {
+            eprintln!("{}", e);
+            HttpResponse::InternalServerError().body("Failed to save short link")
+        }
+    }
 }
 
 #[get("/")]
--- avel/src/main.rs
+++ avel/src/main.rs
@@ -1,10 +1,14 @@
 mod fns;
 
 use std::env;
+use std::sync::Arc;
 use actix_web::{App, HttpServer};
 use actix_web::middleware::Logger;
-use sqlx::{Pool, Postgres, query};
+use actix_web::web::Data;
+use sqlx::{PgPool, Pool, Postgres, query};
+use log::info;
 use crate::fns::{hello, save_short_link, short_link};
+
 
 pub struct AppState {
     pub cockroachdb_session: CockroachDBSession
@@ -14,24 +18,29 @@ type CockroachDBSession = Pool<Postgres>;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-
     env::set_var("RUST_LOG", "debug");
     env_logger::init();
 
-    let conn_url = "postgresql://root@localhost:26257/defaultdb?sslmode=disable";
+    let conn_url = "postgresql://user:password@cockroachdb-client-secure.default.pod.cluster.local:26257/defaultdb";
 
-    let connection: CockroachDBSession = sqlx::PgPool::connect(&conn_url).await.unwrap();
-
+    let connection: CockroachDBSession = PgPool::connect(&conn_url).await.unwrap();
     queries(&connection).await;
 
-    HttpServer::new(|| {
+    let app_state = Data::new(Arc::new(AppState {
+        cockroachdb_session: connection
+    }));
+
+    info!("Successfully started server on 0.0.0.0:5000");
+
+    HttpServer::new(move || {
         App::new()
             .wrap(Logger::default())
+            .app_data(app_state.clone())
             .service(hello)
             .service(short_link)
             .service(save_short_link)
     })
-        .bind(("0.0.0.0", 8080))?
+        .bind(("0.0.0.0", 5000))?
         .run()
         .await
 }
@@ -39,7 +48,7 @@ async fn main() -> std::io::Result<()> {
 
 async fn queries(connection: &CockroachDBSession) {
     let b = query(
-        "CREATE TABLE IF NOT EXISTS pen(c Text);"
+        "CREATE TABLE IF NOT EXISTS links(short_link TEXT, full_link TEXT, PRIMARY KEY(short_link, full_link));"
     );
 
     b.execute(connection).await.unwrap();
