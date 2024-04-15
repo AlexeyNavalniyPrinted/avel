24a665c38c081e95986c71d5713e83ec19f79913
AlexeyNavalniyPrinted <117517842+AlexeyNavalniyPrinted@users.noreply.github.com>
2024-04-15 17:05:52 +0300

    Connected data source (Check if possible to do by code), auto deploy python script.

---
+++ auto.py
@@ -0,0 +1,71 @@
+import subprocess
+import time
+
+def run_command(command):
+    print(f"Executing command: {command}")
+    process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    stdout, stderr = process.communicate()
+    if process.returncode != 0:
+        if command.startswith("helm install") and "cannot re-use a name that is still in use" in stderr.decode("utf-8"):
+            print(f"{command} / Error: cannot re-use a name that is still in use ")
+            return stdout.decode("utf-8")
+        print(f"Error executing command: {command}")
+        print(f"Error message: {stderr.decode('utf-8')}")
+        exit(1)
+    print(f"Command executed successfully: {command}")
+    return stdout.decode('utf-8')
+
+def main():
+    print("Starting script execution...")
+
+    print("Applying cert-manager...")
+    run_command("kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.14.4/cert-manager.yaml")
+
+    print("Waiting for cert-manager deployments...")
+    for deployment in ["cert-manager", "cert-manager-cainjector", "cert-manager-webhook"]:
+        run_command(f"kubectl wait --for=condition=available --timeout=600s deployment/{deployment} -n cert-manager")
+
+    print("Applying CockroachDB issuer...")
+    run_command("kubectl apply -f cockroach/cockroach_issuer.yaml")
+
+    print("Adding Helm repositories and updating...")
+    run_command("helm repo add cockroachdb https://charts.cockroachdb.com/")
+    run_command("helm repo add grafana https://grafana.github.io/helm-charts")
+    run_command("helm repo update")
+
+    print("Installing CockroachDB...")
+    run_command("helm install cockroach --values cockroach/cockroach_values.yaml cockroachdb/cockroachdb")
+
+    print("Labeling CockroachDB service for Prometheus...")
+    run_command("kubectl label svc cockroach-cockroachdb prometheus=cockroachdb")
+
+    print("Applying Prometheus Operator bundle and CockroachDB-specific Prometheus configurations...")
+    run_command("kubectl apply -f https://raw.githubusercontent.com/prometheus-operator/prometheus-operator/v0.73.1/bundle.yaml --server-side")
+    run_command("kubectl apply -f cockroach/prometheus.yaml")
+
+    print("Creating secret for Alertmanager configuration...")
+    run_command("kubectl create secret generic alertmanager-cockroachdb --from-file=alertmanager.yaml=cockroach/alertmanager-config.yaml")
+    run_command("kubectl label secret alertmanager-cockroachdb app=cockroachdb")
+
+    print("Applying Alertmanager configurations...")
+    run_command("kubectl apply -f https://raw.githubusercontent.com/cockroachdb/cockroach/master/cloud/kubernetes/prometheus/alertmanager.yaml")
+    run_command("kubectl apply -f https://raw.githubusercontent.com/cockroachdb/cockroach/master/cloud/kubernetes/prometheus/alert-rules.yaml")
+
+    print("Installing Grafana...")
+    run_command("helm install grafana grafana/grafana")
+
+    print("Applying CockroachDB secure configuration...")
+    run_command("kubectl apply -f cockroach/cockroach-secure.yaml")
+
+    print("Executing SQL commands to create a user and a table...")
+    run_command("kubectl exec -it cockroachdb-client-secure -- cockroach sql --certs-dir=./cockroach-certs --host=cockroach-cockroachdb-public -e \"CREATE USER roach WITH LOGIN PASSWORD 'password'; CREATE TABLE IF NOT EXISTS links(short_link TEXT, full_link TEXT, last_accessed TIMESTAMP, PRIMARY KEY(short_link)); GRANT SELECT, INSERT, UPDATE, DELETE ON TABLE links TO roach;\"")
+    print("Installing Redis...")
+    run_command("helm install redis oci://registry-1.docker.io/bitnamicharts/redis")
+
+    print("Applying the application YAML file...")
+    run_command("kubectl apply -f app.yaml")
+
+    print("Script execution completed successfully.")
+
+if __name__ == "__main__":
+    main()
--- backlog.md
+++ backlog.md
@@ -1,13 +1,10 @@
 <------------------------------------------Todo------------------------------------------>
-1. Connect grafana to data
-2. Fix alert manager connection to cockroach db
-3. Permissions in containers
+1. Fix alert manager connection to cockroach db /* I HAVE NO FUCKING (Don't use 'FUCKING', it's profane. (by 'alex' style guide)) CLUE HOW */ 
+2. Permissions in containers
+3. Add monitoring to everything
 4. Add tests
-
-<------------------------------------------New------------------------------------------>
-1. Learn about terraform and ansible and check if I can integrate them
-2. Any Ci/Cd tool
-3. Learn about istio and check if I can integrate it
-4. Learn about Jaeger and check if I can integrate it
-5. Learn about Sentry and check if I can integrate it
-6. Add monitoring to all services
+5. Learn about terraform and ansible and check if I can integrate them
+6. Any Ci/Cd tool
+7. Learn about istio and check if I can integrate it
+8. Learn about Jaeger and check if I can integrate it
+9. Learn about Sentry and check if I can integrate it
